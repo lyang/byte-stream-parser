811045f7c209b4e7e808e0afd2dd21b9031c595b
Lin Yang <github@linyang.me>
2024-11-27 23:11:00 -0800

    Add BinaryParser

---
+++ src/main/java/com/github/lyang/bytestreamparser/scalar/util/InputStreams.java
@@ -0,0 +1,31 @@
+package com.github.lyang.bytestreamparser.scalar.util;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+public final class InputStreams {
+  private static final String END_OF_BYTE_STREAM_REACHED =
+      "End of stream reached after reading %d bytes, bytes expected %d";
+
+    private InputStreams() {}
+
+  public static byte[] readFully(InputStream input, int length) throws IOException {
+    byte[] bytes = new byte[length];
+    int total = 0;
+    while (total < length) {
+      int read = input.read(bytes, total, length - total);
+      if (read == -1) {
+        break;
+      } else {
+        total += read;
+      }
+    }
+    if (total != length) {
+      throw new EOFException(String.format(END_OF_BYTE_STREAM_REACHED, total, length));
+    }
+    return bytes;
+  }
+}
